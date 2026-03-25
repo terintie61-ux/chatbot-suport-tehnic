d55e4665896de8f6ce6f0e3e7b9c780ac96b4ea2
Dumitru <dima.vasilache28@gmail.com>
2026-03-26 01:42:22 +0200

    Create Flask REST API server template in backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,29 @@
 #Flask REST-API chatbot
+
+# Install flask for running the server:
+# pip install flask
+from flask import Flask,request
+
+
+
+PATH_MODEL = ""
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, I am a Chatbot which offer help for culinary recipes"
+
+
+# Process the message of the user send to the chatbot
+@app.route('/chat', methods=['POST'])
+def chat():
+    pass
+
+
+def load_model():
+    pass
+
+
+
+app.run()
